ab06cc2f2f78c901975fa871ed8a35da80531b4b
Asonna <Asonna>
2017-02-10 04:52:38 -0800

    add models folder with products file

---
+++ src/GummiBearKingdom/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummiBearKingdom.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int cost { get; set; }
+        public string OriginCountry { get; set; }
+
+
+    }
+}
