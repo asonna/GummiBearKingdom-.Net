741a95bbe971f74c688caa1dda3729e6f030d61d
Asonna <Asonna>
2017-02-13 01:22:36 -0800

    add Readme file;and additional styling

--- src/GummiBearKingdom/Controllers/SectionsController.cs
+++ src/GummiBearKingdom/Controllers/SectionsController.cs
@@ -11,20 +11,14 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GummiBearKingdom.Controllers
 {
-    public class ProductsController : Controller
+    public class SectionsController : Controller
     {
         private GummiBearDbContext db = new GummiBearDbContext();
 
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View(db.Products.Include(products => products.Section).ToList());
-        }
-
-        public IActionResult Details(int id)
-        {
-            var thisProduct = db.Products.Include(pr => pr.Section).FirstOrDefault(products => products.ProductId == id);
-            return View(thisProduct);
+            return View(db.Sections.ToList());
         }
 
         public IActionResult Create()
@@ -34,9 +28,9 @@ namespace GummiBearKingdom.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Product product)
+        public IActionResult Create(Section section)
         {
-            db.Products.Add(product);
+            db.Sections.Add(section);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
@@ -44,15 +38,15 @@ namespace GummiBearKingdom.Controllers
 
         public IActionResult Edit(int id)
         {
-            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            var thisSection = db.Sections.FirstOrDefault(sections => sections.SectionId == id);
             ViewBag.SectionId = new SelectList(db.Sections, "SectionId", "Name");
-            return View(thisProduct);
+            return View(thisSection);
         }
 
         [HttpPost]
-        public IActionResult Edit(Product product)
+        public IActionResult Edit(Section section)
         {
-            db.Entry(product).State = EntityState.Modified;
+            db.Entry(section).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
@@ -60,18 +54,27 @@ namespace GummiBearKingdom.Controllers
 
         public IActionResult Delete(int id)
         {
-            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
-            return View(thisProduct);
+            var thisSection = db.Sections.FirstOrDefault(sections => sections.SectionId == id);
+            return View(thisSection);
         }
 
         [HttpPost, ActionName("Delete")]
         public IActionResult DeleteConfirmed(int id)
         {
-            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
-            db.Products.Remove(thisProduct);
+            var thisSection = db.Sections.FirstOrDefault(sections => sections.SectionId == id);
+            db.Sections.Remove(thisSection);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult ProdutsInSection (int id)
+        {
+            //var thisProduct = db.Sections.FirstOrDefault(sections => sections.SectionId == id);
+            //return View(thisProduct);
+            return View(db.Products.Include(products => products.SectionId).ToList());
+        }
+
+        
 
     }
 }
